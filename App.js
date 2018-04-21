d3d9c33e95b320af5d0fa5895331db5e0dd089f1
Michael Cheung <michaelc1993@gmail.com>
2018-04-21 14:35:53 -0700

    create templates

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Welcome to the Trivia Game! Tap BEGIN to start.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 20
+  }
+});
---
+++ src/components/screensComponent/homeScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    
+  }
+}
